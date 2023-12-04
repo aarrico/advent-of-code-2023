e17d50e23ef0e50bf40c378c912f56eb8324cb1f
Alexander Arrico <alex.arrico@gmail.com>
2023-12-03 21:00:01 -0800

    complete day 2 part 1

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,25 +1,27 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::time::{Instant};
+use std::cmp::Ordering;
+
+//use std::time::{Instant};
 
 const INPUT_FILE_PATH: &str = "inputs/input.txt";
 
-#[derive(Debug)]
-struct Round {
+#[derive(Debug, Eq, Ord)]
+struct CubeSet {
     blue: u16,
     red: u16,
     green: u16,
 }
 
-impl Round {
-    fn new(round_str: &str) -> Self {
-        let round = round_str.split(',');
+impl CubeSet {
+    fn parse_from_str(set_str: &str) -> Self {
+        let set = set_str.split(',');
         let mut blue: u16 = 0;
         let mut red: u16 = 0;
         let mut green: u16 = 0;
 
-        for cube in round {
+        for cube in set {
             let cube_data: Vec<&str> = cube.split_whitespace().collect();
 
             if cube_data.len() != 2 {
@@ -30,7 +32,7 @@ impl Round {
                 match cube_data[0].parse::<u16>() {
                     Ok(count) => count,
                     Err(err) =>
-                        panic!("couldn't process cube count for round {}:\n\terr: {}", round_str, err)
+                        panic!("couldn't process cube count for round {}:\n\terr: {}", set_str, err)
                 };
 
             match cube_data[1] {
@@ -48,23 +50,47 @@ impl Round {
         }
     }
 
-    fn parse_rounds(rounds_str: &str) -> Vec<Round> {
-        let rounds_to_parse: Vec<&str> = rounds_str.split(";").collect();
+    fn parse_sets_from_str(sets_str: &str) -> Vec<CubeSet> {
+        let rounds_to_parse: Vec<&str> = sets_str.split(";").collect();
 
-        let mut games: Vec<Round> = Vec::new();
+        let mut sets: Vec<CubeSet> = Vec::new();
 
         for g in rounds_to_parse {
-            games.push(Round::new(g));
+            sets.push(CubeSet::parse_from_str(g));
         }
 
-        games
+        sets
+    }
+}
+
+impl PartialEq<Self> for CubeSet {
+    fn eq(&self, other: &Self) -> bool {
+        &self.blue == &other.blue
+            && &self.red == &other.red
+            && &self.green == &other.green
+    }
+}
+
+impl PartialOrd for CubeSet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.red < other.red && self.blue < other.blue && self.green < other.green {
+            return Some(Ordering::Less);
+        } else if self.red > other.red && self.blue > other.blue && self.green > other.green {
+            return Some(Ordering::Greater);
+        } else if self.red > other.red || self.green > other.green || self.blue > other.blue {
+            return Some(Ordering::Greater);
+        } else if self == other {
+            return Some(Ordering::Equal);
+        }
+
+        return Some(self.cmp(&other));
     }
 }
 
 #[derive(Debug)]
 struct GameData {
     game_num: u16,
-    rounds: Vec<Round>,
+    rounds: Vec<CubeSet>,
 }
 
 fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>> where P: AsRef<Path>, {
@@ -84,7 +110,7 @@ fn parse_line(to_split: &String) -> GameData {
     if let Ok(game_num) = game_num_str[5..].parse::<u16>() {
         GameData {
             game_num,
-            rounds: Round::parse_rounds(&rounds[1..]),
+            rounds: CubeSet::parse_sets_from_str(&rounds[1..]),
         }
     } else {
         panic!("couldn't process game num for\n\t{}\n\t", to_split)
@@ -105,19 +131,38 @@ fn populate_game_data(lines: io::Lines<io::BufReader<File>>) -> Vec<GameData> {
     games
 }
 
-fn find_sum() -> u16 {
+fn is_game_possible(game_data: &GameData, cubes_to_check: &CubeSet) -> bool {
+    for round in &game_data.rounds[0..] {
+        if round > cubes_to_check {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn find_sum(cubes_to_check: &CubeSet) -> u16 {
     let mut sum: u16 = 0;
     let lines = read_lines(Path::new(INPUT_FILE_PATH));
 
     let games = populate_game_data(lines);
     for g in games {
-        println!("{:?}", g);
+        if is_game_possible(&g, cubes_to_check) {
+            sum += g.game_num;
+        }
     }
 
     sum
 }
 
 fn main() {
-    let sum = find_sum();
+    //12 red, 13 green, 14 blue
+    let part1_cubes = CubeSet {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
+
+    let sum = find_sum(&part1_cubes);
     println!("{}", sum)
 }
