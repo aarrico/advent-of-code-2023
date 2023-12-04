c3538e383fd0deea5c5c2accab87d43462d405d8
Alexander Arrico <alex.arrico@gmail.com>
2023-12-03 23:16:01 -0800

    chore(day-3): read input into matrix

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+#[derive(Debug)]
+enum MatrixObj {
+    Number(u8),
+    Symbol(char),
+    Period,
+}
+
+fn determine_type(c: char) -> MatrixObj {
+    if c.is_ascii_digit() {
+        return MatrixObj::Number(c as u8);
+    } else if c != '.' {
+        return MatrixObj::Symbol(c);
+    }
+
+    return MatrixObj::Period;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = BufReader::new(File::open("input.txt").unwrap());
+    let mut line = String::new();
+
+    file.read_line(&mut line).unwrap();
+
+    let matrix: Vec<Vec<MatrixObj>> =
+        file.lines()
+            .map(|line| line.unwrap().chars()
+                .map(|x| determine_type(x)).collect()).collect();
 }
